693b99c66c7bf7c462c0cbc0fc8a064d8210fbe0
jackmeyer <jackmeyer@localhost>
2024-09-04 15:22:25 -0500

    Good work team!

---
+++ src/studio1/Average.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("enter integer #1:");
+	int n1 = in.nextInt();
+	System.out.println("enter integer #2:");
+	int n2 = in.nextInt();
+	double i = (n1+n2);
+	System.out.println(i/2);
+
+	}
+
+}
